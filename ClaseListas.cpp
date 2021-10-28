f7ba5b9f044f31c6c427dd42d340e743853b88cd
LINO ABRAHAM AVILA ZUNIGA <lino.avila@unah.edu.hn>
2021-10-27 19:57:35 -0600

    actualizacion 10272021
    se agregaron el destructor y funciones imprime, insertainicio y esvacia .

--- ClaseListas.cpp
+++ ClaseListas.cpp
@@ -24,20 +24,77 @@ public:
         primero=NULL;
     }
 ~lista();
-void ins_primero(int v);
+void insert_inicio(int v);
+bool esvacia();
 void ins_final(int v);
 void borrar_primero();
 void borrar_final();
-
+void Imprime();
 void borrar_vref(int vref);
 void ins_lista_n(int , int);
-
 };
+lista::~lista()
+{
+    pnodo T,Q=primero;
+    while(Q!=NULL)
+    {
+        T=Q;
+        Q=Q->siguiente;
+        delete T;
+    }
+}
+
+void lista::insert_inicio(int v)
+{
+    pnodo nuevo;
+    nuevo= new  nodo(v, primero );
+    primero=nuevo;
+}
+
+bool lista::esvacia()
+{
+return primero==NULL;    
+}
+
 int main()
 {
- //prueba de actualizacion del codigo
 
-    
+lista lista1;/// se ha creado la lista vacia
 
+for(int i=0;i<10;i++)
+{
+    lista1.insert_inicio(i);
+}
+lista1.Imprime();
+
+lista lista2;
+if(lista2.esvacia())
+    cout<<"lista 2 esta vacia";
     return 0;
 }
+
+
+
+
+
+void lista::Imprime()
+{
+ if( !esvacia())
+ {
+     pnodo Q=primero;
+     cout<<"Primero->"<<Q->valor;
+    while(Q->siguiente!=NULL )
+    {
+        Q=Q->siguiente;
+        if(Q->siguiente==NULL || Q==primero)
+             cout<<"->"<<Q->valor<<"->NULL"<<endl;
+        else
+             cout<<"->"<<Q->valor;
+    }
+ }
+ else
+ cout<<"La lista esta vacia";
+
+}
+
+
